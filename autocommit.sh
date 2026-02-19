59d61f7be8df961951c4ee0513b2d7ecc21d1a5c
Mbarabba <nicholas.scorrano@gmail.com>
2026-02-19 15:15:01 +0100

    Backup automatico . 19-02-2026 15:15

---
+++ autocommit.sh
@@ -0,0 +1,13 @@
+cd /home/nicholas/Desktop/Uni || exit 
+
+# Controllo di modifiche
+if [[ -z $(git status -s) ]]; then
+  echo "Nessuna modifica"
+  exit 0
+fi
+
+# Faccio il commit
+git add .
+timestamp=$(date +"%d-%m-%Y %H:%M")
+git commit -m "Backup automatico . $timestamp"
+git push origin master
